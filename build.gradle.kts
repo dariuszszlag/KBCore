47d093873d620f51cea8a6e2ab166ba3564c0dd1
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-04-20 15:38:55 +0200

    Downgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library").version("8.0.0").apply(false)
+    id("com.android.library").version("7.4.1").apply(false)
     kotlin("multiplatform").version("1.8.20").apply(false)
 }
 
