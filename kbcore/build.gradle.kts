d29970c44b2c1d81e175d26edca5093c2b180cc8
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-05-26 00:24:04 +0200

    Bump version of the publishing library to 0.0.30

--- kbcore/build.gradle.kts
+++ kbcore/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("multiplatform")
     id("com.android.library")
     id("maven-publish")
-    id("com.dariusz.multiplatform-swiftpackage") version "0.0.16"
+    id("com.dariusz.multiplatform-swiftpackage") version "0.0.30"
 }
 
 group = "com.darek"
