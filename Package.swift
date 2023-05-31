2ec2e035eb7a1126eea099fc767b27d7cf5399ae
dariuszszlag <dariuszszlag@users.noreply.github.com>
2023-05-31 21:14:08 +0000

    Package.swift for v0.3.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "kbcore",
-            url: "https://maven.pkg.github.com/dariuszszlag/KBCore/com/darek/kbcore-multi/0.3.0/kbcore-multi-0.3.0.zip",
-            checksum: "5987fc027b2b6ff349388cd03bfd6a2709400877ceb8fb33a5b84b676ae6c8ed"
+            url: "https://maven.pkg.github.com/dariuszszlag/KBCore/com/darek/kbcore-multi/0.3.1/kbcore-multi-0.3.1.zip",
+            checksum: "735913110194f9df765b5472037881b702fed3e122d43ed160442840045ef12a"
         ),
     ]
 )
