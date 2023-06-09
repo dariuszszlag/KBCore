e253c7eb6574d21cc707dbe37781e7681e3bd039
Dariusz Szlag <dariusz.szlag@appunite.com>
2023-06-09 18:07:35 +0200

    Downgradle libs

--- kbcore/build.gradle.kts
+++ kbcore/build.gradle.kts
@@ -37,11 +37,11 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
-                implementation("io.ktor:ktor-client-core:2.3.1")
-                implementation("io.ktor:ktor-client-mock:2.3.1")
-                implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.1")
-                implementation("io.ktor:ktor-client-content-negotiation:2.3.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                implementation("io.ktor:ktor-client-core:2.2.4")
+                implementation("io.ktor:ktor-client-mock:2.2.4")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.4")
+                implementation("io.ktor:ktor-client-content-negotiation:2.2.4")
             }
         }
         val commonTest by getting {
@@ -52,7 +52,7 @@ kotlin {
         val androidMain by getting {
             dependsOn(commonMain)
             dependencies {
-                implementation("io.ktor:ktor-client-android:2.3.1")
+                implementation("io.ktor:ktor-client-android:2.2.4")
             }
         }
         val androidUnitTest by getting {
@@ -67,7 +67,7 @@ kotlin {
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
             dependencies {
-                implementation("io.ktor:ktor-client-darwin:2.3.1")
+                implementation("io.ktor:ktor-client-darwin:2.2.4")
             }
         }
         val iosX64Test by getting
@@ -124,5 +124,5 @@ multiplatformSwiftPackage {
         iOS { v("16") }
     }
     buildConfiguration { release() }
-    distributionMode { maven() }
+    distributionMode { local() }
 }
